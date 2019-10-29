87b946a64a370a5ea1a9df3d234a598c50bcfa1d
Shehzad Aslam <shehzad.aslam@powersoft19.com>
2019-10-29 22:29:06 +0500

    privacy for mobile app

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,10 +1,16 @@
 import Home from '../views/home';
+import Privacy from '../views/privacy';
 
 const appRoutes = [
     {
         path: "/",
         title: "Shehzad Aslam",
         component: Home
+    },
+    {
+        path: "/privacy",
+        title: "SA 4k privacy",
+        component: Privacy
     },
     {
         redirect: true,
